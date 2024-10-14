cb24726c85ad3f2545b5e18596a70d2b0dbd5d39
afnan030503 <muhammadafnanmaulidi@gmail.com>
2024-10-14 11:46:59 +0700

    melakukan pengecekan

---
+++ app/src/main/java/com/example/bab1/ConditionalExpresion.kt
@@ -0,0 +1,14 @@
+package com.example.bab1
+
+fun ConditionalStatement(){
+    println("=== Conditional Expression ===")
+
+
+    // If-Else
+    val number = 10
+    if (number > 0){
+        println("Positive number")
+    } else {
+        println("Negative number")
+    }
+}
--- app/src/main/java/com/example/bab1/Nullsafety.kt
+++ app/src/main/java/com/example/bab1/Nullsafety.kt
@@ -30,5 +30,5 @@ println(strLength(neverNull))
 }
 
 fun main(){
-    NullSafety
+    NullSafety()
 }
