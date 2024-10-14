9c5c1573452817ab2e5b505d87cf7a4b7d128c58
afnan030503 <muhammadafnanmaulidi@gmail.com>
2024-10-14 11:34:24 +0700

    dalam progeam kotlin membuat variabel dan mencetak ke layar

--- app/src/main/java/com/example/bab1/class.kt
+++ app/src/main/java/com/example/bab1/class.kt
@@ -1,3 +1,10 @@
 package com.example.bab1
 
 class Contact(val id: Int, var email: String)
+
+fun main(){
+    val contact = Contact(1,"Mary@gmail.com")
+
+    //print the value of the property: email
+    println(contact.email)
+}
