7ed1bf554a6b0a3bc37555df561cf67bd9ccd9f3
afnan030503 <muhammadafnanmaulidi@gmail.com>
2024-10-14 11:41:59 +0700

    cara kotlin menghindari kesalahan Nullsafety

---
+++ app/src/main/java/com/example/bab1/Nullsafety.kt
@@ -0,0 +1,30 @@
+package com.example.bab1
+
+fun NullSafety(){
+    var neverNull: String = "This can't be null"
+
+
+    // Nullable has nullable string type
+    var nullable: String? = "You can a null here"
+    nullable = null
+
+
+    // Check for null in conditions
+    if(neverNull == null){
+        println("inferredNonNull is Null")
+    } else {
+        println("inferredNonNull is not Null")
+    }
+
+
+    // Safe call operator
+    println(neverNull.length) //18
+    println(nullable?.length) //null
+
+
+// notNull doesn't accept null values
+fun strLength(notNull: String) : Int{
+    return notNull.length
+}
+println(strLength(neverNull))
+}
