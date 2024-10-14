80fbf73563f05831b22d5870b14e932eaef88851
afnan030503 <muhammadafnanmaulidi@gmail.com>
2024-10-14 11:51:58 +0700

    mendeklarasikan day dan jika tidak ada dalam list tersebut maka invalid

--- app/src/main/java/com/example/bab1/ConditionalExpresion.kt
+++ app/src/main/java/com/example/bab1/ConditionalExpresion.kt
@@ -13,5 +13,14 @@ fun ConditionalStatement(){
     }
     //when
     val day = 1
-    when (day) =
+    when (day){
+        1 -> println("Minggu")
+        2 -> println("Senin")
+        3 -> println("Selasa")
+        4-> println("Rabu")
+        5-> println("Kamis")
+        6-> println("Jumat")
+        7-> println("Sabtu")
+        else ->println("Invalid")
+    }
 }
