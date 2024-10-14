2305d2dda67f8e21bcf127a0b43d2034fd57c22f
afnan030503 <muhammadafnanmaulidi@gmail.com>
2024-10-14 12:11:53 +0700

    mengubah karnan eror

--- app/src/main/java/com/example/bab1/function.kt
+++ app/src/main/java/com/example/bab1/function.kt
@@ -5,4 +5,41 @@ fun withoutParameter() {
     println("Hello, world")
 }
 
-fun withParameter(name: String)
+fun withParameter(name: String){
+    println()
+    println("=== withParameter")
+    println("Hello, $name!")
+}
+
+
+// named argument
+// named argument adalah cara untuk memanggil fungsi dengan menyebutkan nama parameter
+fun withNamedArgument(name: String, age: Int){
+    println()
+    println("=== withNamedArgument")
+    println("Hello, $name!, You are $age years old.")
+}
+
+
+// default parameter value
+// default parameter value merupakan cara untuk memberikan nilai default pada parameter fungsi
+fun withDefaultParameter(name: String= " Indra", age: Int){
+    println()
+    println("=== withDefaultParameter ===")
+    println("Hello,$name! You are $age years old")
+}
+fun withReturn(panjang: Int, lebar: Int): Int {
+    val hasil = panjang * lebar
+    println(hasil)
+    return hasil
+}
+
+
+fun main(){
+    withoutParameter()
+    withParameter("John")
+
+
+    withNamedArgument(name = "John", age= 30)
+    withDefaultParameter(age = 25)
+}
