86edf4af92fbf90425677b361a9f2ef89ce0a1bb
afnan030503 <muhammadafnanmaulidi@gmail.com>
2024-10-14 11:37:31 +0700

    nilai email diubah , dan di cetak ke layar

--- app/src/main/java/com/example/bab1/class.kt
+++ app/src/main/java/com/example/bab1/class.kt
@@ -2,9 +2,13 @@ package com.example.bab1
 
 class Contact(val id: Int, var email: String)
 
-fun main(){
-    val contact = Contact(1,"Mary@gmail.com")
+fun main() {
+    val contact = Contact(1, "Mary@gmail.com")
 
     //print the value of the property: email
+    println(contact.email)
+
+    //update the value of the property: email
+    contact.email = "jane@gmail.com"
     println(contact.email)
 }
